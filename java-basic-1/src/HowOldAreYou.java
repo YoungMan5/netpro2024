1534dfef5433f085b5f7b673fa0d6ace3de432d6
GitHub <22fi125@ms.dendai.ac.jp>
2024-04-24 21:31:14 +0900

    kadai add

--- java-basic-1/src/HowOldAreYou.java
+++ java-basic-1/src/HowOldAreYou.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class HowOldAreYou {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			// BufferedReader というのは、データ読み込みのクラス(型)
@@ -13,21 +13,42 @@ public class HowOldAreYou {
 
 			// readLine() は、入出力エラーの可能性がある。エラー処理がないとコンパイルできない。
 			//  Java では、 try{ XXXXXXXX }  catch(エラーの型 変数) { XXXXXXXXXXXXXXXXXX} と書く
+	while (true) {
 		try {
-			System.out.println("何歳ですか?");
+			System.out.println("何歳ですか?(qまたはeで終了)");
 			String line = reader.readLine();
+			if (line.equalsIgnoreCase("q") || line.equalsIgnoreCase("e")) {
+				System.out.println("終了します");
+				break;
+			}
 			int age = Integer.parseInt(line);
+			if (age < 0 || age >= 120) {
+				System.out.println("正しい年齢を入力してください。");
+				continue;
+			} 
 			System.out.println("あなたは" + age + "歳ですね。");
-			System.out.println("あなたは10年後、" + (age + 10) + "歳ですね。");
+			System.out.println("あなたは2030年、" + (age + 2030-2024) + "歳ですね。");
+			int era = 2024 - age;
+			String eraName;
+			if (era >= 1868 && era <= 1911) {
+				eraName = "明治" + (era - 1867) + "年";
+			} else if (era == 1912) {
+				eraName = "大正" + (era - 1911) + "年";
+			} else if (era >= 1926 && era <= 1988) {
+				eraName = "昭和" + (era - 1925) + "年";
+			} else if (era >= 1989 && era <= 2018) {
+				eraName = "平成" + (era - 1988) + "年";
+			} else if (era >= 2019) {
+				eraName = "令和" + (era - 2018) + "年";
+			} else {
+				eraName = "エラー";
+			}
+			System.out.println("あなたが誕生した元号は,"+ eraName +"ですね。");
 		}
 		catch(IOException e) {
 			System.out.println(e);
 		}
-
+	}
 
 	}
 }
-
-//  課題    キーボードから数字を打ち込む
-//  その結果、 あなたは、???歳ですね、と画面に表示させる。
-//  その後、あなたは10年後、????歳ですね、と画面に表示させる。
